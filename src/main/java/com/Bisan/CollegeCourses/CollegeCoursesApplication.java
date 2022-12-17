7cb245730028c4286f10a41679d2bc685e95bade
BisanGhoul <bbisann@gmail.com>
2022-12-17 11:50:28 +0200

    initial commit

---
+++ src/main/java/com/Bisan/CollegeCourses/CollegeCoursesApplication.java
@@ -0,0 +1,14 @@
+package com.Bisan.CollegeCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeCoursesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CollegeCoursesApplication.class, args);
+	}
+
+}
